39e14293baa261df073713f6c21b94b3e083f8f9
darknight <illuminating.me@gmail.com>
2026-04-07 09:47:57 +0800

    feat(web-front): wire all components into final dashboard layout

--- web-front/src/App.tsx
+++ web-front/src/App.tsx
@@ -1,12 +1,20 @@
-import { ConfigProvider, theme, Layout, Typography } from 'antd'
+import { ConfigProvider, theme, Layout, Typography, Divider, Spin } from 'antd'
 import { useThemeStore } from './stores/useThemeStore'
+import { useDashboardStore } from './stores/useDashboardStore'
 import { usePolling } from './hooks/usePolling'
 import ThemeToggle from './components/ThemeToggle'
+import KpiCards from './components/KpiCards'
+import BalanceChart from './components/BalanceChart'
+import PositionValueChart from './components/PositionValueChart'
+import DayIncomeChart from './components/DayIncomeChart'
+import BigLossTable from './components/BigLossTable'
+import HistoryTable from './components/HistoryTable'
 
 const { Header, Content } = Layout
 
 export default function App() {
   const isDark = useThemeStore((s) => s.isDark)
+  const loading = useDashboardStore((s) => s.loading)
   usePolling()
 
   return (
@@ -27,8 +35,29 @@ export default function App() {
           </Typography.Title>
           <ThemeToggle />
         </Header>
-        <Content style={{ padding: 24 }}>
-          {/* Components will be wired in final assembly */}
+        <Content style={{ padding: 24, maxWidth: 1400, margin: '0 auto', width: '100%' }}>
+          {loading ? (
+            <div style={{ textAlign: 'center', padding: 100 }}>
+              <Spin size="large" />
+            </div>
+          ) : (
+            <>
+              <KpiCards />
+
+              <Divider>余额和持仓价值</Divider>
+              <BalanceChart />
+              <PositionValueChart />
+
+              <Divider>净利润日变化</Divider>
+              <DayIncomeChart />
+
+              <Divider>大额亏损交易</Divider>
+              <BigLossTable />
+
+              <Divider>历史数据</Divider>
+              <HistoryTable />
+            </>
+          )}
         </Content>
       </Layout>
     </ConfigProvider>
